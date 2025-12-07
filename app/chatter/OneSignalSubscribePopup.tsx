a80eb6ec8f7592f20bc06b893114b08ec83d3c42
infro <infromane@gmail.com>
2025-12-07 17:59:07 +0200

    fix

--- app/chatter/OneSignalSubscribePopup.tsx
+++ app/chatter/OneSignalSubscribePopup.tsx
@@ -88,7 +88,7 @@ export default function SubscribePopup({ onChangeCallState, email, chatId }) {
 
   const willDisplayBackRejectCallMessage = function (e) {
     
-    if(e.data.type == 'cancel_call') {
+    if(e.data.action == 'cancel_call') {
       onChangeCallState('IDLE');
     }
   }
